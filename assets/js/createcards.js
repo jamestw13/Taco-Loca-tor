e619498ca127e0e2313ffa504336d82fc30abb6b
Victor Smith <victorj500@gmail.com>
2021-10-09 13:26:39 -0500

    randomized card images

--- assets/js/createcards.js
+++ assets/js/createcards.js
@@ -1,30 +1,30 @@
 var locationFormEl = document.querySelector("#locale-form");
 
 let tacos = [
-  'assets/images/taco1.jpg', 
-  'assets/images/taco2.jpg', 
-  'assets/images/taco3.jpg', 
-  'assets/images/taco4.jpg', 
-  'assets/images/taco5.jpg' 
+  'assets/images/taco1.jpg',
+  'assets/images/taco2.jpg',
+  'assets/images/taco3.jpg',
+  'assets/images/taco4.jpg',
+  'assets/images/taco5.jpg'
 ]
 console.log(tacos)
-  testDataChicago = {
-    restaurant_name: "Tamale Foodie",
-    restaurant_phone: "(312) 577-7014",
-    restaurant_website: "",
-    hours: "",
-    price_range: "$$$$$",
-    price_range_num: 5,
-    restaurant_id: 4188276687636707,
-    cuisines: ["Latin American", "Mexican", "Other"],
-    address: {
-      city: "Chicago",
-      state: "IL",
-      postal_code: "60612",
-      street: "Location Varies",
-      formatted: "Location Varies Chicago, IL 60612",
-    },
-  }
+testDataChicago = {
+  restaurant_name: "Tamale Foodie",
+  restaurant_phone: "(312) 577-7014",
+  restaurant_website: "",
+  hours: "",
+  price_range: "$$$$$",
+  price_range_num: 5,
+  restaurant_id: 4188276687636707,
+  cuisines: ["Latin American", "Mexican", "Other"],
+  address: {
+    city: "Chicago",
+    state: "IL",
+    postal_code: "60612",
+    street: "Location Varies",
+    formatted: "Location Varies Chicago, IL 60612",
+  },
+}
 
 
 var formSubmitHandler = function (event) {
@@ -51,16 +51,22 @@ var formSubmitHandler = function (event) {
   //console.log(price_range);
   newEl.appendChild(price);
 
-  for(var i= 0, l = tacos.length; i< l; i++){
-    console.log(tacos[i+1]);
-  }
-
   //create image element
-  var img = document.createElement("img");
-  img.src = "assets/images/taco4.jpg";
-  img.classList="image";
-  //console.log(price_range);
-  newEl.appendChild(img);
+
+    
+    var img = document.createElement("img");
+    //const random = Math.floor(Math.random()* tacos.length); 
+    //img.src = tacos[random]
+    img.src = tacos[0];
+    tacos.shift();
+
+    img.classList = "image";
+    //console.log(price_range);
+    newEl.appendChild(img);
+  
+
+
+
 
 
 
